cfc2998957337c7728af0764a0753af5b3e6aad0
Gareth Whitley <me@garethwhitley.online>
2021-03-16 20:03:13 +0000

    removed redundant response models

--- TimeSharerApi/ResponseModels/OpportunityResponseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace TimeSharerApi.Models
-{
-    public class OpportunityResponseModel : BaseResponseModel
-    {
-        public List<Opportunity> Data { get; set; }
-    }
-}
--- TimeSharerApi/ResponseModels/VolunteerResponseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-namespace TimeSharerApi.Models
-{
-    public class VolunteersResponseModel : BaseResponseModel
-    {
-        public List<Volunteer> Data { get; set; } = new List<Volunteer>();
-    }
-}
